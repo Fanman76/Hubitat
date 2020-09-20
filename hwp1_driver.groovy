312fa539888309b13830f2c475f9c6ed4acbd517
Fanman76 <48988697+Fanman76@users.noreply.github.com>
2020-09-20 11:36:00 +0200

    Update hwp1_driver.groovy

--- hwp1_driver.groovy
+++ hwp1_driver.groovy
@@ -6,13 +6,26 @@
 
 metadata
 {
-    definition(name: "HomeWizard P1 Meter", namespace: "fan", author: "Fanman-76", importUrl: "")
+    definition(name: "HomeWizard P1 Meter", namespace: "fan", author: "Fanman-76", importUrl: "https://github.com/Fanman76/Hubitat-HomeWizard_P1/blob/master/hwp1_driver.groovy")
     {
         capability "Initialize"
         capability "PowerMeter"
         capability "Refresh"
         
         attribute "commStatus", "string"
+        attribute "smr_version", "string"
+        attribute "meter_model", "string"
+        attribute "wifi_ssid", "string"
+        attribute "wifi_strength", "string"
+        attribute "total_power_import_t1_kwh", "number"
+        attribute "total_power_import_t2_kwh", "number"
+        attribute "total_power_import_t3_kwh", "number"
+		attribute "active_power_w", "number"
+		attribute "active_power_l1_w", "string"
+		attribute "active_power_l2_w", "string"
+		attribute "active_power_l3_w", "string"
+		attribute "total_gas_m3", "number"
+		attribute "gas_timestamp", "date"
     }
 }
 
@@ -42,9 +55,21 @@ def updated()
 def initialize()
 {
     sendEvent(name: "commStatus", value: "unknown")
-    sendEvent(name: "power", value: "unknown")
+    sendEvent(name: "active_power_w", value: "unknown")
     sendEvent(name: "WiFi-Strength", value: "unknown")
+    sendEvent(name: "smr_version", value: "unknown")
+    sendEvent(name: "meter_model", value: "unknown")
+    sendEvent(name: "wifi_ssid", value: "unknown")
+    sendEvent(name: "wifi_strength", value: "unknown")
     sendEvent(name: "total_power_import_t1_kwh", value: "unknown")
+    sendEvent(name: "total_power_import_t2_kwh", value: "unknown")
+    sendEvent(name: "total_power_export_t1_kwh", value: "unknown")
+    sendEvent(name: "total_power_export_t2_kwh", value: "unknown")
+    sendEvent(name: "active_power_l1_w", value: "unknown")
+    sendEvent(name: "active_power_l2_w", value: "unknown")
+    sendEvent(name: "active_power_l3_w", value: "unknown")
+    sendEvent(name: "total_gas_m3", value: "unknown")
+    sendEvent(name: "gas_timestamp", value: "unknown")
     refresh()
 }
 
@@ -56,9 +81,20 @@ def refresh()
     {
         def res = httpGetExec([uri: getBaseURI()], true)
         sendEvent(name: "commStatus", value: "good")
-        sendEvent(name: "power", value: res?.active_power_w?.toInteger())
-        sendEvent(name: "WiFi-Strength", value: res?.wifi_strength.toInteger())
+        sendEvent(name: "smr_version", value: res?.smr_version.toInteger())
+        sendEvent(name: "meter_model", value: res?.meter_model.toString())
+        sendEvent(name: "wifi_ssid", value: res?.wifi_ssid.toString())
+        sendEvent(name: "wifi_strength", value: res?.wifi_strength.toInteger())
         sendEvent(name: "total_power_import_t1_kwh", value: res?.total_power_import_t1_kwh.toInteger())
+        sendEvent(name: "total_power_import_t2_kwh", value: res?.total_power_import_t2_kwh.toInteger())
+        sendEvent(name: "total_power_export_t1_kwh", value: res?.total_power_export_t1_kwh.toInteger())
+        sendEvent(name: "total_power_export_t2_kwh", value: res?.total_power_export_t1_kwh.toInteger())
+        sendEvent(name: "active_power_w", value: res?.active_power_w?.toInteger())
+        sendEvent(name: "active_power_l1_w", value: res?.active_power_l1_w.toString())
+        sendEvent(name: "active_power_l2_w", value: res?.active_power_l2_w.toString())
+        sendEvent(name: "active_power_l3_w", value: res?.active_power_l3_w.toString())
+        sendEvent(name: "total_gas_m3", value: res?.total_gas_m3.toInteger())
+        sendEvent(name: "gas_timestamp", value: res?.gas_timestamp.toInteger())
         
         // schedule next refresh
         runIn(refreshInterval.toInteger(), refresh)
