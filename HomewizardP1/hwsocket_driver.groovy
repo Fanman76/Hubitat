9f3b8dd79626710bdc885894b0a4c4796017dc2e
The Fanman <48988697+Fanman76@users.noreply.github.com>
2021-11-13 15:19:01 +0100

    Partial re-write of code
    Now supporting device info (firmware, api version, product details etc)

--- HomewizardP1/hwsocket_driver.groovy
+++ HomewizardP1/hwsocket_driver.groovy
@@ -1,6 +1,8 @@
 /*
     Hubitat driver for the HomeWizard Power Socket meter.
+    (c) Fanman76 11-2021
  */
+import groovy.json.JsonSlurper
 
 metadata
 {
@@ -8,144 +10,105 @@ metadata
     {
         capability "Initialize"
         capability "PowerMeter"
-        capability "Refresh"
         
+        attribute "product_type", "string"
+        attribute "product_name", "string"
+        attribute "serial", "string"
+        attribute "firmware_version", "string"
+        attribute "api_version", "string"
         attribute "wifi_ssid", "string"
         attribute "wifi_strength", "number"
         attribute "total_power_import_t1_kwh", "number"
         attribute "total_power_export_t1_kwh", "number"
         attribute "active_power_l1_w", "number"
-        attribute "power", "number"
+        attribute "power", "number"        
+        
+        command "getBasicInfo"
+        command "getUsageInfo"
+        command "unschedule"
     }
 }
-
 preferences
 {
     section
     {
         input "ipAddress", "text", title: "IP address meter", required: true
-        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
-        input ( name: 'pollInterval', type: 'enum', title: 'Update interval (in minutes)', options: ['1', '5', '10', '15', '30', '60', '180'], required: true, defaultValue: '60' )
-	    input name: "enablePoll", type: "bool", title: "Enable device polling", defaultValue: false
+        input name: "enablePoll", type: "bool", title: "Enable device polling", defaultValue: false
+        input ( name: 'pollInterval', type: 'enum', title: 'Update interval (in minutes)', options: ['1', '5', '10', '15', '30', '60', '180'], required: true, defaultValue: '60' )   
     }
 }
 
-def logDebug(msg) 
+def unschedule()
 {
-    if (logEnable)
-    {
-        log.debug(msg)
-    }
+    unschedule()
 }
 
-def updated()
+void getBasicInfo()
 {
-    refresh()
+    String DeviceInfoURI = "http://${ipAddress}/api" 
+ 
+    httpGet([uri:DeviceInfoURI, contentType: "application/json"])
+
+	{ resp->
+			def contentType = resp.headers['content-type']
+        
+        sendEvent(name: "product_type", value: resp.data.product_type.toString())
+        sendEvent(name: "product_name", value: resp.data.product_name.toString())
+        sendEvent(name: "serial", value: resp.data.serial.toString())
+        sendEvent(name: "firmware_version", value: resp.data.firmware_version.toString())
+        sendEvent(name: "api_version", value: resp.data.api_version.toString())
+   } 
+}
+
+void getUsageInfo()
+{
+    String DeviceInfoURI = "http://${ipAddress}/api/v1/data" 
+ 
+    httpGet([uri:DeviceInfoURI, contentType: "application/json"])
+
+	{ resp->
+			def contentType = resp.headers['content-type']
+
+        sendEvent(name: "wifi_ssid", value: resp.data.wifi_ssid.toString())
+        sendEvent([name: "wifi_strength", value: resp.data.wifi_strength.toInteger(), unit: "%"])
+        sendEvent([name: "total_power_import_t1_kwh", value: resp.data.total_power_import_t1_kwh.toInteger(), unit: "kWh"])
+        sendEvent([name: "total_power_export_t1_kwh", value: resp.data.total_power_export_t1_kwh.toInteger(), unit: "kWh"])
+        sendEvent([name: "power", value: resp.data.active_power_w.toInteger(), unit: "W"])
+   } 
 }
 
 def initialize()
 {
-    sendEvent(name: "wifi_strength", value: "unknown")
-    sendEvent(name: "wifi_ssid", value: "unknown")
-    sendEvent(name: "total_power_import_t1_kwh", value: "unknown")
-    sendEvent(name: "total_power_export_t1_kwh", value: "unknown")
-    sendEvent(name: "active_power_l1_w", value: "unknown")
-    sendEvent(name: "power", value: "unknown")
-
-    unschedule()
-    refresh()
-}
-
-def refresh()
-{
-
-    try
-    {
-        def res = httpGetExec([uri: getBaseURI()], true)
-        sendEvent(name: "wifi_ssid", value: res?.wifi_ssid.toString())
-        sendEvent([name: "wifi_strength", value: res?.wifi_strength.toInteger(), unit: "%"])
-        sendEvent([name: "total_power_import_t1_kwh", value: res?.total_power_import_t1_kwh.toInteger(), unit: "kWh"])
-        sendEvent([name: "total_power_export_t1_kwh", value: res?.total_power_export_t1_kwh.toInteger(), unit: "kWh"])
-        sendEvent([name: "power", value: res?.active_power_w.toInteger(), unit: "W"])
-        
-     if (res) { 
-       if (res.active_power_l1_w != "NULL") {
-         sendEvent([name: "active_power_l1_w", value: res?.active_power_l1_w.toInteger(), unit: "W"])
-      }
-    }  
-
-        
     if (enablePoll) {
-	
 		Integer interval = Integer.parseInt(settings.pollInterval)
 		
 		switch (interval) {
 			case 1: 
-				runEvery1Minute(refresh)
+				runEvery1Minute(getUsageInfo)
 				break
 			case 5:
-				runEvery5Minutes(refresh)
+				runEvery5Minutes(getUsageInfo)
 				break
 			case 10:
-				runEvery10Minutes(refresh)
+				runEvery10Minutes(getUsageInfo)
 				break
 			case 15:
-				runEvery15Minutes(refresh)
+				runEvery15Minutes(getUsageInfo)
 				break
 			case 30:
-				runEvery30Minutes(refresh)
+				runEvery30Minutes(getUsageInfo)
 				break
 			case 60:
-				runEvery1Hour(refresh)
+				runEvery1Hour(getUsageInfo)
 				break
 			case 180:
-				runEvery3Hours(refresh)
+				runEvery3Hours(getUsageInfo)
 				break
 			default:
-				runIn(interval*60,refresh)
+				runIn(interval*60,getUsageInfo)
 				break
 		}
 		
-	    logDebug("Scheduled to run ${interval} minutes") 
-		runIn(2, getBaseURI) //  Run after updates in addition to the scheduled poll
+		runIn(2, getUsageInfo) //  Run after updates in addition to the scheduled poll
 	}
-    }
-    catch (Exception e)
-    {
-        logDebug("refresh() failed: ${e.message}")
-        logDebug("run Refresh command re-start polling")
-        sendEvent(name: "commStatus", value: "error")
-    }    
-}
-
-def getBaseURI()
-{
-    return "http://${ipAddress}/api/v1/data"
-}
-
-def httpGetExec(params, throwToCaller = false)
-{
-    logDebug("httpGetExec(${params})")
-    
-    try
-    {
-        def result
-        httpGet(params)
-        { resp ->
-            if (resp.data)
-            {
-//                logDebug("resp.data = ${resp.data}")
-                result = resp.data
-            }
-        }
-        return result
-    }
-    catch (Exception e)
-    {
-        logDebug("httpGetExec() failed: ${e.message}")
-        if(throwToCaller)
-        {
-            throw(e)
-        }
-    }
 }
